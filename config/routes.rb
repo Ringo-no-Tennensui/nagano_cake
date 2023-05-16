eaded3dd276e66ce571ce5a1cc67e64beb565657
jun0033 <129253550+jun0033@users.noreply.github.com>
2023-05-16 07:28:23 +0000

    Create routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,32 @@
 Rails.application.routes.draw do
 
-　　end
+  # 管理者側のルーティング設定
+  namespace :admin do
+    resources :orders, only:[:show, :update]
+    resources :order_details, only:[:update]
+    resources :customers, only:[:index, :show, :edit, :update]
+    resources :genres, only:[:index, :edit, :create, :update]
+    resources :items, only:[:index, :new, :show, :edit, :create, :update]
+    root to: 'homes#top'
+  end
+
+  # 会員側のルーティング設定
+  scope module: :public do
+    resources :shipping_addresses, only:[:index, :edit, :create, :update, :destroy]
+    resources :orders, only:[:new, :index, :show, :create]
+    post 'orders/confirm'
+    get 'orders/thanks'
+    resources :cart_items, only:[:index, :create, :update, :destroy]
+    delete 'cart_items/destroy_all'
+    resource :customers, only:[:edit, :update]
+    get 'customers/mypage' =>'customers#show'
+    get 'customers/confirm'
+    patch 'customers/withdraw'
+    resources :items, only:[:show, :index]
+    root to: 'homes#top'
+    get '/about' =>'homes#about'
+  end
+
 # 顧客用
 # URL /customers/sign_in ...
 devise_for :customers,skip: [:passwords], controllers: {
