c2f745b022d60c2b0264a3140bd393121e45c00c
jun0033 <129253550+jun0033@users.noreply.github.com>
2023-05-16 09:10:02 +0000

    operation check

--- app/controllers/public/registrations_controller.rb
+++ app/controllers/public/registrations_controller.rb
@@ -6,12 +6,12 @@ class Public::RegistrationsController < Devise::RegistrationsController
 
   # GET /resource/sign_up
   def new
-    super 
+
   end
 
   #POST /resource
   def create
-    super
+
   end
 
   # GET /resource/edit
