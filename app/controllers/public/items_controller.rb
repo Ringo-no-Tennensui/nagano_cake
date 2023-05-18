c4021425ab4af7bdfde6e9f2b2c25aae8e324ee6
11as22 <ec2-user@ip-172-31-3-73.ap-northeast-1.compute.internal>
2023-05-18 05:52:51 +0000

    [create] public item index

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -1,7 +1,15 @@
 class Public::ItemsController < ApplicationController
+  
   def index
+    @items = Item.page(params[:page]).per(8)
+    @genres = ItemGenre.all  
   end
-
+  
   def show
+    @item = Item.find(params[:id])
+    @genres = ItemGenre.all
+    @cart_items = CartItem.new()
   end
 end
+
+
