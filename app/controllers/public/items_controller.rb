19a925535a3b31463ba9272c244b7de06c20b95f
11as22 <ec2-user@ip-172-31-3-73.ap-northeast-1.compute.internal>
2023-05-18 06:52:08 +0000

    調整

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -1,14 +1,15 @@
 class Public::ItemsController < ApplicationController
   
   def index
+   # @item = Item.find(params[:id])
     @items = Item.page(params[:page]).per(8)
     @genres = ItemGenre.all  
   end
   
   def show
-    @item = Item.find(params[:id])
+    #@item = Item.find(params[:id])
     @genres = ItemGenre.all
-    @cart_items = CartItem.new()
+    #@cart_items = CartItem.new
   end
 end
 
