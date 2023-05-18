e850db034636d166dab9bfd7dca2a74990fbdb9e
jun0033 <129253550+jun0033@users.noreply.github.com>
2023-05-18 06:51:34 +0000

    [create]genre_index,item_new

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -1,13 +1,31 @@
 class Admin::GenresController < ApplicationController
+
   def index
+    @genres = ItemGenre.new
+    @genres = ItemGenre.all
   end
-  
+
   def create
+  @genres = ItemGenre.new(genre_params)
+    if @genres.save
+      # create機能を発火させた元の画面にもどす
+      redirect_to request.referer
+    else
+      @genres = ItemGenre.all
+      rener :index
+    end
   end
 
   def edit
   end
-  
+
   def update
   end
+
+  private
+
+  def genre_params
+  params.require(:item_genre).permit(:genre)
+  end
+
 end
