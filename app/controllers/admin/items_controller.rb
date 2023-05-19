e0ce4665f399b8bdc1afd52f9506154a6f5de647
jun0033 <129253550+jun0033@users.noreply.github.com>
2023-05-19 07:56:55 +0000

    [create]item_edit,show,customer_edit,show

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -24,14 +24,18 @@ class Admin::ItemsController < ApplicationController
   @item = Item.find(params[:id])
   end
 
+  def edit
+  @item = Item.find(params[:id])
+  end
+
   def update
   @item = Item.find(params[:id])
     if @item.update(item_params)
-      flash[:notice] = "#{@item.name}を編集しました"
-      redirect_to admin_item_path(@item.id)
+      flash[:notice] = "#{@item.item_name}を編集しました"
+      redirect_to admin_item_path(@item)
     else
       flash[:alert] = "編集に失敗しました"
-      redirect_to edit_admin_item_path(@item)
+      render :edit
     end
   end
 
