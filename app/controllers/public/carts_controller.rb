62eb7dd5b8b06da7c577784c08bef4492041753a
11as22 <ec2-user@ip-172-31-3-73.ap-northeast-1.compute.internal>
2023-05-19 08:11:08 +0000

    アイテム追加調整中

--- app/controllers/public/carts_controller.rb
+++ app/controllers/public/carts_controller.rb
@@ -1,18 +1,29 @@
 class Public::CartsController < ApplicationController
   def index
-    @carts = Cart.all
+    @carts = current_customer.carts
     # カート内の合計金額 @total
     @total = @carts.inject(0) { |sum, cart| sum + cart.subtotal }
   end
 
   def create
     @cart = Cart.new(cart_params)
-    @cart.save
+    if current_customer.carts.find_by(@cart.item_id)
+      
+      
+      redirect_to items_path
+      else
+      @cart.save
     redirect_to items_path
+    end
   end
 
   def update
+    @cart= Cart.find(params[:id])
+    @cart.update(cart_params)
+    redirect_to carts_path
+
   end
+
 
   def destroy
     cart = Cart.find(params[:id])
@@ -21,10 +32,13 @@ class Public::CartsController < ApplicationController
   end
 
   def destroy_all
-    cart = Cart.all
-    cart.destroy
+    current_customer.carts.destroy_all
     redirect_to carts_path
+    #cart = Cart.all
+    #cart.destroy
+    #redirect_to carts_path
   end
+
 
   private
 
