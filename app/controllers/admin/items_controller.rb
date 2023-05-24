55a66acd352a5878626b2dff87f2b24d020b3098
11as22 <ec2-user@ip-172-31-3-73.ap-northeast-1.compute.internal>
2023-05-24 08:48:12 +0000

    リーダブルコード　　説明文の追加

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,17 +1,20 @@
 class Admin::ItemsController < ApplicationController
 
   def index
-  @items = Item.page(params[:page])
+    #ページネーションの設定
+    @items = Item.page(params[:page]) 
   end
 
   def new
-  @item = Item.new
+    @item = Item.new
   end
 
   def create
-  @item = Item.new(item_params)
+    @item = Item.new(item_params)
     if @item.save
-      redirect_to admin_item_path(@item)
+      #追加したアイテムの詳細ページ
+      redirect_to admin_item_path(@item) 
+      #追加したアイテム名を表示
       flash[:notice] = "#{@item.item_name}を追加しました"
     else
       flash[:alert] = "追加に失敗しました"
@@ -21,17 +24,19 @@ class Admin::ItemsController < ApplicationController
   end
 
   def show
-  @item = Item.find(params[:id])
+    @item = Item.find(params[:id])
   end
 
   def edit
-  @item = Item.find(params[:id])
+    @item = Item.find(params[:id])
   end
 
   def update
-  @item = Item.find(params[:id])
+    @item = Item.find(params[:id])
     if @item.update(item_params)
-      flash[:notice] = "#{@item.item_name}を編集しました"
+      #編集したアイテム名を表示
+      flash[:notice] = "#{@item.item_name}を編集しました" 
+      #編集したアイテムの詳細画面に飛ぶ
       redirect_to admin_item_path(@item)
     else
       flash[:alert] = "編集に失敗しました"
@@ -42,7 +47,7 @@ class Admin::ItemsController < ApplicationController
   private
 
   def item_params
-  params.require(:item).permit(:item_name, :image, :item_explanation, :item_price, :sales_status, :item_genre_id)
+    params.require(:item).permit(:item_name, :image, :item_explanation, :item_price, :sales_status, :item_genre_id)
   end
 
 end
