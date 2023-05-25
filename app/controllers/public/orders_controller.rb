bbd5d5cb7e2ad35591a8489f156c8e5c13979b26
11as22 <ec2-user@ip-172-31-3-73.ap-northeast-1.compute.internal>
2023-05-25 08:49:54 +0000

    admin 検索ステータス表示色　　item詳細レイアウト調整

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -1,86 +1,94 @@
 class Public::OrdersController < ApplicationController
-  before_action :authenticate_customer!
   def new
-    cart_items = current_customer.cart_items
-    if cart_items.present?
+    @carts = current_customer.carts
+    # カートの中に商品が入っている場合
+    if @carts.any?
       @order = Order.new
-      @shipping_addresses = ShippingAddress.all
+      @shipping_addresses = current_customer.shipping_addresses.all
+    # カートの中に商品がない場合オーダーには進めない
     else
-      flash[:notice] = "・カートが空です"
-      redirect_to request.referer
+    #今現在のページ
+    redirect_to carts_path
     end
   end
 
   def create
     @order = Order.new(order_params)
-    @order.customer_id = current_customer.id
-   if @order.save
-      current_customer.cart_items.each do |cart_item|
-        @order_item = OrderItem.new
-        @order_item.order_id =  @order.id
-        @order_item.item_id = cart_item.item_id
-        @order_item.count = cart_item.count
-        @order_item.order_price = (cart_item.item.price*1.1).floor
-        @order_item.work_status = 0
-        @order_item.save
+    if @order.save
+      # カートの情報をオーダーディティールに複製
+      current_customer.carts.each do |cart|
+        order_detail = OrderDetail.new()
+        order_detail.order_id = @order.id
+        order_detail.item_id = cart.item_id
+        order_detail.quantity = cart.count
+        order_detail.item_price = cart.item.tax_price
+        order_detail.save
       end
-      current_customer.cart_items.destroy_all
-      redirect_to orders_completed_path
-   else
-     @cart_items = current_customer.cart_items
-     render :confirm
-   end
+      # カートの中身を削除
+      current_customer.carts.destroy_all
+      redirect_to thanks_orders_path
+    end
   end
 
   def confirm
+    # @orderはでかい箱で、その中に小さい箱を指定するためにストロングパラメーターを指定している。
     @order = Order.new(order_params)
-    if params[:order][:select_address] == "0"
-      @order.delivery_post_code = current_customer.post_code
-      @order.delivery_address = current_customer.address
-      @order.delivery_address_label = current_customer.first_name + current_customer.last_name
-    elsif params[:order][:select_address] == "1"
-      @shipping_address = ShippingAddress.find(params[:order][:shipping_address_id])
-      @order.delivery_post_code = @shipping_address.post_code
-      @order.delivery_address = @shipping_address.address
-      @order.delivery_address_label = @shipping_address.address_label
-    elsif params[:order][:select_address] == "2"
-      @order.delivery_post_code = params[:order][:post_code]
-      @order.delivery_address = params[:order][:address]
-      @order.delivery_address_label = params[:order][:address_label]
-      @order.customer_id = current_customer.id
+      # if文を記述して、hidden fieldが作動するようにする。
+      # ご自身の住所と配送先住所が選択された場合はhiddenで処理
+      # 現在memberに登録されている住所であれば
+    if params[:order][:address_option] == "0"
+      @order.ships_post_number = current_customer.post_code
+      @order.ships_address = current_customer.address
+      @order.ships_name = current_customer.last_name + current_customer.first_name
+    # collection.selectであれば
+    elsif params[:order][:address_option] == "1"
+      ship = ShippingAddress.find_by(params[:order][:customer_id])
+      # 配送先住所を登録していないのであれば１のラジオボタンを押しても先には進めない
+      if current_customer.shipping_addresses.exists?
+      @order.ships_post_number = ship.ships_post_number
+      @order.ships_address = ship.ships_address
+      @order.ships_name = ship.ships_name
+      end
+    # 新規住所入力であれば
+    elsif params[:order][:address_option] == "2"
+      @order.ships_post_number = params[:order][:ships_post_number]
+      @order.ships_address = params[:order][:ships_address]
+      @order.ships_name = params[:order][:ships_name]
+    else
+      render 'new'
     end
-    @cart_items = current_customer.cart_items
-
-   if @order.delivery_post_code && @order.delivery_address && @order.delivery_address_label && @order.pay_option
-     if @order.delivery_post_code =~ /\A[0-9]{7}\z/
+    @carts = current_customer.carts.all
+    @order.customer_id = current_customer.id
+    
+    if @order.ships_post_number && @order.ships_address && @order.ships_name && @order.payment
+      # 郵便番号（ハイフンなし7桁）
+      if @order.ships_post_number =~ /\A[0-9]{7}\z/
        render :confirm
-     else 
+      else
        flash[:notice] = "・郵便番号が正しくありません"
        redirect_to request.referer
-     end
-   else
-     flash[:notice] = "・未入力の項目があります"
-     redirect_to request.referer
-   end
-   
+      end
+    else
+    flash[:notice] = "・未入力の項目があります"
+    redirect_to request.referer
+    end
   end
 
-  def completed
+  def thanks
   end
 
   def index
-    @orders=current_customer.orders.all.page(params[:page]).per(10)
-
+    @orders = current_customer.orders
   end
 
   def show
     @order = Order.find(params[:id])
-    @order_items = OrderItem.where(order_id: params[:id])
+    # sumの初期値を0にしてから小計を加算
+    @total = @order.order_details.inject(0) { |sum, order_detail| sum + order_detail.total_price }
   end
 
-  private
-
-  def order_params
-    params.require(:order).permit(:customer_id, :shipping_fee, :total_price, :pay_option, :delivery_post_code, :delivery_address, :delivery_address_label, :order_status)
-  end
+ private
+    def order_params
+        params.require(:order).permit(:customer_id, :ships_address, :ships_post_number, :ships_name, :payment, :shipping_cost, :pay_price, :order_status)
+    end
 end
